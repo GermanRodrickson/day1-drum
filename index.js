61b004ee55cad305ce72038686b5d5f07bc7e1f8
GermanRodrickson <germanrodrickson@gmail.com>
2018-12-29 14:19:16 +0100

    done

--- index.js
+++ index.js
@@ -3,20 +3,14 @@ const s = document.getElementById("S");
 const d = document.getElementById("D");
 const audioA = new Audio("./audio/Slap-SoundMaster13-49669815.mp3");
 
-document.onkeydown = function (event) {
-  switch (event.keyCode) {
-    case 65:
-      a.classList.add("active");
-      // audioA.start();
-    break;
-    case 83:
-      s.classList.add("active");
-    break;
-    case 68:
-      d.classList.add("active");
-    break;
-  }
-}
+window.addEventListener('keydown', function (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const box = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if(!audio) return;
+  audio.currentTime=0;
+  audio.play();
+  box.classList.add("active")
+})
 
 document.onkeyup = (event) => {
   switch (event.keyCode) {
