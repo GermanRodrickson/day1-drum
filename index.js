b7290569116bcd61b61c25f1075322a68d997197
GermanRodrickson <germanrodrickson@gmail.com>
2018-12-29 13:14:48 +0100

    audio

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-const A = document.getElementById("A");
+const a = document.getElementById("A");
+const s = document.getElementById("S");
+const d = document.getElementById("D");
+const audioA = new Audio("./audio/Slap-SoundMaster13-49669815.mp3");
 
 document.onkeydown = function (event) {
-  event.preventDefault();
-
   switch (event.keyCode) {
     case 65:
-      A.classList.add("active");
-      break;
+      a.classList.add("active");
+      // audioA.start();
+    break;
+    case 83:
+      s.classList.add("active");
+    break;
+    case 68:
+      d.classList.add("active");
+    break;
+  }
+}
+
+document.onkeyup = (event) => {
+  switch (event.keyCode) {
+    case 65:
+      a.classList.remove("active");
+    break;
+    case 83:
+      s.classList.remove("active");
+    break;
+    case 68:
+      d.classList.remove("active");
+    break;
   }
 }
