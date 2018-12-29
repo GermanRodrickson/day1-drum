6a91a274ad194d886346f5a30453eeee3edffad5
GermanRodrickson <germanrodrickson@gmail.com>
2018-12-29 12:49:28 +0100

    onkey

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hola d");
+const A = document.getElementById("A");
+
+document.onkeydown = function (event) {
+  event.preventDefault();
+
+  switch (event.keyCode) {
+    case 65:
+      A.classList.add("active");
+      break;
+  }
+}
